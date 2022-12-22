e5484160c975f74dc3d4bc8204be56dc7abbe501
Bor P <golovanovaea.7@gmail.com>
2022-12-22 18:30:21 +0300

    первая задача

---
+++ HW 2.cs
@@ -0,0 +1,17 @@
+﻿// int number1 = Convert.ToInt32(Console.ReadLine());
+
+// int number2 = Convert.ToInt32(Console.ReadLine());
+
+// if (number1 > number2)
+// System.Console.WriteLine(number1);
+
+// else
+
+// System.Console.WriteLine(number2);
+
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+
+System.Console.WriteLine(Math.Max(number1, number2));
