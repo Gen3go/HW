2fe3f31f9c1261c6c606272bedfe4fc0f79a7fa0
Bor P <golovanovaea.7@gmail.com>
2022-12-23 14:05:17 +0300

    2 задача. не работает

---
+++ HW 4.cs
@@ -0,0 +1,22 @@
+
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int Max = 0;
+
+if (num1 > num2)
+
+  Max = (num1);
+
+else (num2 > num3)
+
+  Max = (num2);
+
+else
+
+    Max = (num3);
+
+System.Console.WriteLine(Max);
