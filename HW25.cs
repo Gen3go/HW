e306d594ddc0b33fe048605122d1a3006ed65d22
Bor P <golovanovaea.7@gmail.com>
2023-01-14 15:51:47 +0300

    Задача решена

---
+++ HW25.cs
@@ -0,0 +1,16 @@
+
+System.Console.WriteLine("Введите число А");
+
+int A = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите число В");
+
+int B = Convert.ToInt32(Console.ReadLine());
+
+if (A!=0 && B!=0){
+
+    System.Console.WriteLine(Math.Pow(A, B));
+
+
+}
+
