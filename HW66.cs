a274b4bbd1cf23fbedb53612d78fb06d2333f714
Bor P <golovanovaea.7@gmail.com>
2023-02-08 20:33:52 +0300

    Задача решена

--- HW66.cs
+++ HW66.cs
@@ -4,23 +4,42 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
-int SummElemenstArray(int[]array, int m, int n)
-    { int summ = 0;
-      array[0] = m;
-        for(int i = 0; i < array.Length; i++)
-            if(array[i] <= n)
-        {
-            summ = array[i] + summ;
-            array[i + 1] = array[i] + 1;
-        }
-        return summ;
-    }
 
+void FillArray (int[] array, int n)
+{
+    Random rd = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rd.Next(1, 100);
+    }
+}
+
+void PrintArray(int[] array, int n)
+{
+    Console.WriteLine(string.Join(",", array));
+}
+
+int SummElemenstArray(int[] array, int m, int n)
+{
+    int summ = 0;
+    
+    for (int i = 0; i < array.Length; i++)
+        {   if (array[i] > m && array[i] < n)
+               summ = array[i] + summ;
+        }
+    return summ;
+}
 
 int m = 1;
 int n = 15;
-int [] array = new int [n];
-int summEl = SummElemenstArray(summ);
+int[] array = new int[n];
+
+FillArray(array, n);
+PrintArray(array, n);
+
+int summEl = SummElemenstArray(array, m, n);
+
 System.Console.WriteLine(summEl);
 
 
